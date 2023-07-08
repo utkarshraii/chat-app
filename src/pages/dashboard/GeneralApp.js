a3533c1555ec894954852935021c53f0df8fe597
utkarshraii <utkarshr348@gmail.com>
2023-07-08 11:40:59 +0530

    4th commit

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,29 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import Chats from "./Chats.js";
+import { Box, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Conversation from "../../components/Conversation/index.js";
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
-    <>
+    <Stack direction={"row"} sx={{ width: "100%" }}>
       {/* chats */}
       <Chats />
-    </>
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw-420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        {/* Conversation */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
