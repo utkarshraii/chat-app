eb50594b5cc19ae23710927108c437e254df40b8
utkarshraii <utkarshr348@gmail.com>
2023-07-17 05:46:37 +0530

    16th
    integrated register and verify api

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,6 +27,7 @@ export default function Router() {
         { element: <RegisterPage />, path: "register" },
         { element: <ResetPasswordPage />, path: "reset-password" },
         { element: <NewPasswordPage />, path: "new-password" },
+        { element: <VerifyPage />, path: "verify" },
       ],
     },
     {
@@ -63,3 +64,4 @@ const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
 const ProfilePage = Loadable(lazy(() => import("../pages/dashboard/Profile")));
+const VerifyPage = Loadable(lazy(() => import("../pages/auth/Verify")));
--- src/sections/auth/RegisterForm.js
+++ src/sections/auth/RegisterForm.js
@@ -12,7 +12,10 @@ import {
 } from "@mui/material";
 import { RHFTextField } from "../../components/hook-form";
 import { Eye, EyeSlash } from "phosphor-react";
+import { RegisterUser } from "../../redux/slices/auth";
+import { useDispatch } from "react-redux";
 const RegisterForm = () => {
+  const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
   const RegisterSchema = Yup.object().shape({
     firstName: Yup.string().required("First Name is required"),
@@ -39,12 +42,13 @@ const RegisterForm = () => {
     reset,
     setError,
     handleSubmit,
-    formState: { errors, isSubmitting, isSubmitSuccessful },
+    formState: { errors },
   } = methods;
 
-  const onSubmit = async (date) => {
+  const onSubmit = async (data) => {
     try {
       //submit data to backend
+      dispatch(RegisterUser(data));
     } catch (error) {
       console.log(error);
       reset();
