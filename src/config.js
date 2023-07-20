67a055f00f1cef6498246aab504b3f2780179b84
utkarshraii <utkarshr348@gmail.com>
2023-07-21 03:10:31 +0530

    fixed url

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import { enUS, frFR, zhCN, viVN, arSD } from "@mui/material/locale";
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
 
-export const BASE_URL = "https://api.chamty.utkhost.online/";
+export const BASE_URL = "https://chamty.utkhost.online/";
 
 export const S3_BUCKET_NAME = "utkarshrai28";
 export const AWS_ACCESS_KEY = "AKIASODBYS7Q4RUXG6U2";
